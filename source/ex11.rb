d4e720039b9355ded055e44467c17d57240b0d72
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-04-30 16:34:13 -0300

    Array - Método Sample

---
+++ source/ex11.rb
@@ -0,0 +1,7 @@
+=begin
+Defina um array com o nome de 5 alunos e utilize algum método para imprimir aleatoriamente o nome de um dos alunos.
+=end
+
+alunos = ["Waliff", "Estevan", "Otho", "João Pedro", "Pedro"]
+
+puts alunos.sample  # O método .sample seleciona aleatoriamente um elemento do array
