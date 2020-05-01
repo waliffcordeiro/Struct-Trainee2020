a5e2c5554d76937cc239d364e7aa5ef738ab6565
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-05-01 11:47:09 -0300

    Do while

--- source/ex7.rb
+++ source/ex7.rb
@@ -1,10 +1,26 @@
 =begin
-Desenvolva um programa que receba do usuário um número entre 1 e 10 e escreva a tabuada do valor lido.
+Desenvolva um programa que receba do usuário um número entre 1 e 10 e escreva a tabuada do valor lido. 
+O programa deve pedir para o usuário inserir um novo número se a entrada não estiver no
+intervalo entre 1 e 10 quantas vezes for preciso.
 =end
 
-print "Digite um número inteiro entre 1 e 10: "
-num = gets.to_i
+num = 0
 
+=begin
+begin
+    print "Digite um número inteiro entre 1 e 10: "
+    num = gets.to_i
+end while(num <= 0 || num > 10)
+=end 
+
+# Garantindo que o número recebido seja entre 1 e 10
+loop do
+    print "Digite um número inteiro entre 1 e 10: "
+    num = gets.to_i
+    break if(num >= 0 && num <= 10)
+end
+
+# Printando a tabuada
 for i in 1..10
     puts ("#{i} * #{num} = #{i * num}")
 end
@@ -12,4 +28,6 @@ end
 =begin
 Perceba que o for é um contador de 1 ao 10 e o número recebido é diretamente o multiplicador que queremos obter
 O produto do contador pelo valor recebido nos retorna diretamente a tabuada que queremos.
+
+Foram feitas duas formas de "do while" no Ruby, ambas formas acima simulam o do while e proporcionam o mesmo resultado
 =end
