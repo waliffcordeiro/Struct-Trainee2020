c9877366d90dad569f6c21a5266a74a483752172
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-05-01 12:45:47 -0300

    Nova chave - Hash

---
+++ source/ex14.rb
@@ -0,0 +1,10 @@
+=begin
+Ainda sobre o hash do exercício 4, crie uma nova chave altura com o valor 1.72. Imprima o hash final gerado.
+=end
+
+aluno1 = {"nome" => "Pedro", "idade" => 20}
+
+# Uma nova chave pode ser inserida através de hash["novachave"] = valor
+aluno1["altura"] = 1.72
+
+puts aluno1
