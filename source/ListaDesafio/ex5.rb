109c03cdd06a3d306054f6ac7d546e9963a60ef1
Waliff Cordeiro <waliff.cordeiro@gmail.com>
2020-05-15 12:52:45 -0300

    Desafio das velas

--- source/ListaDesafio/ex5.rb
+++ source/ListaDesafio/ex5.rb
@@ -12,9 +12,10 @@ velasIniciais = contador = velas
 
 loop do 
     velas = velas/2
+    print(velas)
     # O valor inicial do contador é a quantidade de velas e a cada iteração é somado quantas velas novas foram formadas
     contador += velas
-    break if velas == 1 # Se sobrou apenas 1 vela, não é possível formar uma nova vela
+    break if velas <= 1 # Se sobrou apenas 1 vela, não é possível formar uma nova vela
 end
 
 puts("Você inicialmente tinha #{velasIniciais} que resultou em #{contador} horas de iluminação.")
